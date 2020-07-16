63fba8926f34d83108a8290765aa6a4550d07a26
chenfycc <63218454+chenfycc@users.noreply.github.com>
2020-07-16 19:18:54 +0800

    Update Letalk_Refresher.cpp

--- Letalk_Refresher.cpp
+++ Letalk_Refresher.cpp
@@ -1,5 +1,5 @@
 #include<cstdio>  
-#include<windows.h>    
+#include<windows.h>	
 #define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0)   
 int main()
 {
@@ -9,23 +9,23 @@ int main()
 	{
 		ShowWindow(hwnd,SW_HIDE);
 	}
-    while(1)
+	while(1)
 	{
-        if(KEY_DOWN(116))
+		if(KEY_DOWN(116))
 		{
 			system("taskkill -f -im letalk.exe >nul");
 			system("start \"\" \"C:\\Program Files (x86)\\Strong\\LeTalk\\LeTalk.exe\"");
-        }
-        if(KEY_DOWN(117))
-        {
-        	return 0; 
+		}
+		if(KEY_DOWN(117))
+		{
+			return 0; 
 		} 
-        if(KEY_DOWN(118))
-        {
+		if(KEY_DOWN(118))
+		{
 			system("taskkill -f -im letalk.exe >nul");
-        	return 0; 
+			return 0; 
 		} 
 		_sleep(10); 
-    }
-    return 0;
+	}
+	return 0;
 }
